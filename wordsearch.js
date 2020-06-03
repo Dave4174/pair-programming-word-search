ea0c60e4b7b8150e5e1f7b1618c7e75a68ee6cdf
Dave4174 <dlang4174@gmail.com>
2020-06-03 22:53:16 +0000

    Pair Programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+    if (letters.length === 0) {
+        return false;
+    }
+    
+    // Check horizontally
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    // Transpose the input and check again (which checks vertically)
+    for (let l of transpose(horizontalJoin)) {
+        if (l.join('').includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+const transpose = function(matrix) {
+    let answerArray = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      let rowArray = [];
+      for (let j = 0; j < matrix.length; j ++) {
+        rowArray.push(matrix[j][i]);
+      }
+      answerArray.push(rowArray);
+    }
+  return answerArray;
+  };
+
+module.exports = wordSearch;
